86ee7e9c56b64829d86705c76fdf2c85ce9bb06a
Abner Garbin <abner.batysta151@gmail.com>
2024-11-27 06:25:39 -0300

    Exercicio JPA/Hibernate

---
+++ dominio/Pessoa.java
@@ -0,0 +1,53 @@
+package dominio;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+
+    private Integer id;
+    private String nome;
+    private String email;
+
+    public Pessoa(){
+
+    }
+
+    public Pessoa(Integer id, String email, String nome) {
+        this.id = id;
+        this.email = email;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
